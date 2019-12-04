f1cd7dfcafec137b8c32b73a5f5ee9ef7b3605cd
maro <maro@DESKTOP-6NE5K38>
2019-12-04 15:53:36 +0900

    usr.dir 추가

--- src/main/java/kr/co/shineware/nlp/komoran/test/KomoranFileTest1.java
+++ src/main/java/kr/co/shineware/nlp/komoran/test/KomoranFileTest1.java
@@ -9,7 +9,7 @@ public class KomoranFileTest1 {
 		Komoran komoran = new Komoran("models_light");
 		komoran.setFWDic("user_data/fwd.user");		komoran.setUserDic("user_data/dic.user");
 		
-		String product = "2.LG전자 그램 17 17ZD990-GX30K";
+		String product = "42.레노버 아이디어패드 S145-15 CORE i CLASS";
 		
 		String inputFilename = "resources/input/" + product + ".txt";
 		String outputFilename = "resources/output/result_" + product + ".txt";
