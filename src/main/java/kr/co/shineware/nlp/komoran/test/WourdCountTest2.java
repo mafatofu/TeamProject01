b18fd219bfc90d42edd2587f285cd6b24b3e408e
maro <maro@DESKTOP-6NE5K38>
2019-12-05 17:16:59 +0900

    수정어어어어어

---
+++ src/main/java/kr/co/shineware/nlp/komoran/test/WourdCountTest2.java
@@ -0,0 +1,17 @@
+package kr.co.shineware.nlp.komoran.test;
+
+import kr.co.shineware.util.common.file.FileUtil;
+
+public class WourdCountTest2 {
+		
+	public static void main(String[] args) {
+		
+		String product = "2.LG전자 그램 17 17ZD990-GX30K";
+		
+		String inputFilename = "resources/output/nouns/result_" + product + ".txt";
+		String outputFilename = "resources/output/count/result_" + product + ".txt";
+		FileUtil.FilemapCount(inputFilename, outputFilename, "UTF-8");
+		
+	}		
+}
+	
