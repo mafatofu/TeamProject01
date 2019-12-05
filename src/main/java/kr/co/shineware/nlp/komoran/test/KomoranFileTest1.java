e41d760d4515b07610155f36bd056c10ef739250
KGITBank <KGITBank@DESKTOP-HN411UA>
2019-12-05 11:31:32 +0900

    111111

--- src/main/java/kr/co/shineware/nlp/komoran/test/KomoranFileTest1.java
+++ src/main/java/kr/co/shineware/nlp/komoran/test/KomoranFileTest1.java
@@ -9,21 +9,13 @@ public class KomoranFileTest1 {
 		Komoran komoran = new Komoran("models_light");
 		komoran.setFWDic("user_data/fwd.user");		komoran.setUserDic("user_data/dic.user");
 		
-<<<<<<< HEAD
-		String product = "26.레노버 아이디어패드 S340-15API R5 IPS";
-=======
-		String product = "25.애플 맥북프로 15형 2019년형 MV902KHA";
->>>>>>> branch 'master' of https://github.com/mafatofu/TeamProject01.git
+		String product = "1.LG전자 울트라PC 15U590-GA56K";
 		
 		String inputFilename = "resources/input/" + product + ".txt";
 		String outputFilename = "resources/output/nouns/result_" + product + ".txt";
 		
 		//komoran.analyzeTextFileMorph(inputFilename, outputFilename, 1);
 		komoran.analyzeTextFileNouns(inputFilename, outputFilename, 1);
-<<<<<<< HEAD
-		komoran.analyzeTextFile(inputFilename, outputFilename, 1);
-=======
 		//komoran.analyzeTextFile(inputFilename, outputFilename, 1);
->>>>>>> branch 'master' of https://github.com/mafatofu/TeamProject01.git
 	}
 }
