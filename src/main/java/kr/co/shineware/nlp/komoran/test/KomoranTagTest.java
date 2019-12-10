45929f2c12c5ee83c2fa5542a3e0318b3b78857d
KGITBank <KGITBank@DESKTOP-HN411UA>
2019-12-10 12:54:57 +0900

    KeyWordList 완성(유저 딕셔너리에 단어 추가)

--- src/main/java/kr/co/shineware/nlp/komoran/test/KomoranTagTest.java
+++ src/main/java/kr/co/shineware/nlp/komoran/test/KomoranTagTest.java
@@ -8,13 +8,14 @@ public class KomoranTagTest {
 		Komoran komoran = new Komoran("models_light");
 		komoran.setFWDic("user_data/fwd.user");		komoran.setUserDic("user_data/dic.user");
 		
+		String keyword = "속도";
 		String product = "1.LG전자 울트라PC 15U590-GA56K";
 		String inputFilename = "resources/input/" + product + ".txt";
-		String outputFilename = "resources/output/result_2222" + product + ".txt";
+		String outputFilename = "resources/output/result_2222" + product + "("+keyword+").txt";
 		
 		//komoran.analyzeTextFileMorph(inputFilename, outputFilename, 1);
 		//komoran.analyzeTextFileNouns(inputFilename, outputFilename, 1);
-		komoran.analyzeTextTags(inputFilename, outputFilename, 1, "배송", 
+		komoran.analyzeTextTags(inputFilename, outputFilename, 1, "속도", 
 			"VA", "MAG", "VV", "ETM", "ETN", "SN", "NNB", "JX", "JKB", "NNG", "XSV", "EC", "NNP", "XR", "XSA");
 		//komoran.analyzeTextFile(inputFilename, outputFilename, 1);
 		
