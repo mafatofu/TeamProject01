c822a04acff46e47ec2ea727fafa1c6b5e1057fa
maro <maro@DESKTOP-6NE5K38>
2019-12-10 11:39:04 +0900

    2231

--- src/main/java/kr/co/shineware/nlp/komoran/test/KomoranPNTest.java
+++ src/main/java/kr/co/shineware/nlp/komoran/test/KomoranPNTest.java
@@ -10,7 +10,19 @@ public class KomoranPNTest {
 		String product = "1.LG전자 울트라PC 15U590-GA56K";
 		
 		String inputFilename = "resources/input/" + product + ".txt";
-		String outputFilename = "resources/output/rank/" + product + ".txt";
+		String outputFilename = "resources/output/result_" + product + ".txt";
 		komoran.analyzeTextPN(inputFilename, outputFilename, 1, "shipping");
+		
+	    System.out.println("============긍정=============");  
+		for (String key : CountPNTest.phashmap.keySet()) {
+	          int v = CountPNTest.phashmap.get(key);
+	          System.out.println(key+":"+v);
+	       }
+	       System.out.println("============부정=============");
+	       for (String key : CountPNTest.nhashmap.keySet()) {
+	          int vv = CountPNTest.nhashmap.get(key);
+	          System.out.println(key+":"+vv);
+	          //커밋테스트
+	       }
 	}
 }
