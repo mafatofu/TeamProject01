573d78789a1c986fb8b7a8823754ece683c28ed6
mafatofu <qwqw0310@gmail.com>
2019-12-11 15:50:35 +0900

    12-11

--- src/main/java/kr/co/shineware/nlp/komoran/test/KeyWordList.java
+++ src/main/java/kr/co/shineware/nlp/komoran/test/KeyWordList.java
@@ -40,7 +40,7 @@ public class KeyWordList {
 		}
 	}
 	
-
+   
    //배송키워드 긍정/부정 단어 리스트
    public static final List<String> shipping_positive = new ArrayList<String>
    (Arrays.asList("꼼꼼", "빠르", "안전하게", "바로", "예상", "무료", "하루", "당일", "빨리", "다음날"));
